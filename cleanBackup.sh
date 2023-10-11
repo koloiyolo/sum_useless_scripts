d21a9e7e5ebeb4cfc5e67b6c3b28b65b489da83e
Jakub <jakvb.kolodziej@gmail.com>
2023-10-11 14:41:59 +0200

    Create cleanBackup.sh

---
+++ cleanBackup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+delDate=$(date -d "$(date +"%Y%m%d") - 15 days" +"%Y%m%d")
+
+path=/path/to/backup/folder/*/
+
+dirs=()
+for dir in $path;
+ do
+
+ base=$(basename "$dir")
+ IFS='-' read -ra parts <<< "$base"
+ dirDate=${parts[0]}${parts[1]}${parts[2]}
+
+ if [ $dirDate -lt $delDate ]
+ then
+  rmdir $dir
+#  echo "'$dir' removed"
+ fi
+
+ #dirs+=("$(basename "$dir")")
+ done
+
+#echo ${dirs[@]};
